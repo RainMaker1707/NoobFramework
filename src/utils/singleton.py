c2639627008a995e1a0eb19b8c13ccc5b18c1f64
RainMaker1707 <hadrienallegaert@icloud.com>
2026-09-13 10:29:21 +0200

    [FIX] Singleton has now a metaclass to avoid instance check in unherited object __init__

--- src/utils/singleton.py
+++ src/utils/singleton.py
@@ -1,7 +1,23 @@
-class Singleton:
-    instance = None
+class SingletonMeta(type):
+    _instances = {}
 
-    def __new__(cls):
-        if cls.instance is None:
-            cls.instance = super().__new__(cls)
-        return cls.instance
+    def __call__(cls, *args, **kwargs):
+        if cls is Singleton:
+            raise NotImplementedError(
+                "Singleton is an abstract class that cannot be instantiated directly!"
+            )
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+    """
+        /!\\ Never call super() in unherited object /!\\
+
+    This object is an abstract parent to define a singleton Object by unheritance.
+    Singleton: Allows developper to create object than cannot be re-instanciated
+               If tried to re-instanciate it, it returns the current instanciated object instead.
+    This object is not intended to be directly instanciated and will raise an Error if it is tried.
+    """
+    pass
